22d21584e01c7c7f102c52b3ee0f95ae983e69ce
aiscodes <aigerim6769@gmail.com>
2022-02-05 20:48:02 +0600

    task is done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  let sum = []
+  if (matrix != undefined || matrix) {
+    if (matrix.length != 0) {
+      matrix.map((el, index) => {
+        index == 0 || index % 2 == 0
+          ? sum.push(...el)
+          : sum.push(...el.reverse())
+      })
+      return sum
+    } else {
+      return []
+    }
+  } else {
+    return []
+  }
 }
